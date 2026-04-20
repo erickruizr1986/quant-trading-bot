58ac4021c24b774170a6413eae6e0ceab07f6440
erickruizr1986 <erickruizr@gmail.com>
2026-04-20 18:25:11 -0500

    Update db.py

--- db.py
+++ db.py
@@ -1,11 +1,7 @@
 import sqlite3
-import pandas as pd
-
-DB_NAME = "trades.db"
-
 
 def init_db():
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect("trades.db")
     c = conn.cursor()
 
     c.execute("""
@@ -14,8 +10,6 @@ def init_db():
         symbol TEXT,
         direction TEXT,
         price REAL,
-        strike REAL,
-        premium REAL,
         score REAL
     )
     """)
@@ -24,22 +18,15 @@ def init_db():
     conn.close()
 
 
-def log_trade(symbol, direction, price, strike, premium, score):
+def log_trade(symbol, direction, price, score):
 
-    conn = sqlite3.connect(DB_NAME)
+    conn = sqlite3.connect("trades.db")
     c = conn.cursor()
 
     c.execute("""
-    INSERT INTO trades (symbol, direction, price, strike, premium, score)
-    VALUES (?, ?, ?, ?, ?, ?)
-    """, (symbol, direction, price, strike, premium, score))
+    INSERT INTO trades (symbol, direction, price, score)
+    VALUES (?, ?, ?, ?)
+    """, (symbol, direction, price, score))
 
     conn.commit()
     conn.close()
-
-
-def get_trades():
-    conn = sqlite3.connect(DB_NAME)
-    df = pd.read_sql("SELECT * FROM trades", conn)
-    conn.close()
-    return df
